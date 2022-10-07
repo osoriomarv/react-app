57258def89d953adacd51c6f8324c7f86061394f
osoriomarv <90578878+osoriomarv@users.noreply.github.com>
2022-10-06 19:47:12 -0500

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => (
         Hello, My name is Marvin
         </SectionTitle>
         <SectionText>
-          I am a Graduate Student at the Tulane University of New Orleans. I work in the High Temperature and High Pressure Laboratory (HiTap). By working with computational models I have found a passion for progamming and coding which i hope to explore and develop.
+         I am a Graduate Student at Tulane University pursuing a Master's in Geology. I work at the High Temperature and High Pressure Laboratory (HiTap) as a Research Assistant. I am also a Teaching Assistant for the Geology Department. By working with computation models I have found a passion for programming. This passion led me down a path of self learning using websites like CodeAcademy, Datacamp and sitting in on computer science lectures in my free time. While developing my skills on codeacademy I discoved another passion, Cybersecurity. Since then I have used TryHackMe (top 7%) and HackTheBox to increase my skills.
         </SectionText>
         <Button onClick={() => window.location = 'https://github.com/osoriomarv'}>
           Learn More
